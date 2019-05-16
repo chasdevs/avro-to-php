f26950eb6e0dc11d2842ef92609c6ed01d05ef51
chasdevs <chasdeveas@gmail.com>
2019-05-15 20:50:29 -0400

    Finish compile directory command.

--- app/Commands/CompileDirectory.php
+++ app/Commands/CompileDirectory.php
@@ -2,19 +2,28 @@
 
 namespace App\Commands;
 
-use Illuminate\Console\Scheduling\Schedule;
+use App\Compiler\Compiler;
+use App\Util\Utils;
 use LaravelZero\Framework\Commands\Command;
 
 class CompileDirectory extends Command
 {
 
-    protected $signature = 'compile:directory {directory}';
+    protected $signature = 'compile:directory {directory} {outputDirectory?}';
 
     protected $description = 'Compile a directory containing Avro .avsc files to PHP classes.';
 
-    public function handle()
+    public function handle(Compiler $compiler)
     {
-        $this->error('TODO');
+        $sourceDir = $this->argument('directory');
+        $outputDir = $this->argument('outputDirectory');
+
+        $source = Utils::resolve($sourceDir);
+        $target = Utils::resolve($outputDir);
+
+        $this->comment('Compiling Avro files in ' . $source . ' to ' . $target . '.');
+        $compiler->compile($source, $target);
+        $this->info('Done!');
     }
 
 }
