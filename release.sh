cb70fcc76ab6427260aec50c6a1202ec24612715
chasdevs <chas@storyblocks.com>
2019-10-01 16:24:39 -0400

    Remove docker push from release.

--- release.sh
+++ release.sh
@@ -12,8 +12,3 @@ git add .
 git commit -m "$VER"
 git tag "$VER"
 git push && git push --tag
-
-docker build -t chasdevs/avro-to-php --no-cache .
-docker tag chasdevs/avro-to-php chasdevs/avro-to-php:"$VER"
-docker push chasdevs/avro-to-php:latest
-docker push chasdevs/avro-to-php:"$VER"
