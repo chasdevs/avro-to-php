d767c404d168e077e739b056d02225d641c11ad9
chasdevs <chas@storyblocks.com>
2020-01-29 08:58:27 -0500

    Add parsing of map type.

---
+++ app/Compiler/Avro/AvroMap.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Compiler\Avro;
+
+class AvroMap implements AvroTypeInterface
+{
+
+    /** @var AvroTypeInterface */
+    public $values;
+
+    public function __construct(AvroTypeInterface $values)
+    {
+        $this->values = $values;
+    }
+
+    public static function create(\stdClass $map, ?string $namespace): AvroMap {
+        $type = AvroTypeFactory::create($map->values, $namespace);
+        return new self($type);
+    }
+
+    public function getPhpType(): string
+    {
+        return 'array';
+    }
+
+    public function getPhpDocType(): string
+    {
+        return $this->getPhpType();
+    }
+
+    public function getType(): AvroType
+    {
+        return AvroType::MAP();
+    }
+
+    public function getImports(): array
+    {
+        return $this->values->getImports();
+    }
+
+}
