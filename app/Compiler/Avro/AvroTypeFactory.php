8d1380e6b93502e307173a94c57b1e9e473fbad8
chasdevs <chas@storyblocks.com>
2020-02-21 17:09:01 -0500

    Add namespace map to enable avsc references to qualified record names.

--- app/Compiler/Avro/AvroTypeFactory.php
+++ app/Compiler/Avro/AvroTypeFactory.php
@@ -7,11 +7,12 @@ use App\Compiler\Errors\NotImplementedException;
 class AvroTypeFactory {
 
     private static $recordCache = [];
+    private static $namespaceCache = [];
 
     public static function create($avsc, ?string $namespace = null): AvroTypeInterface {
         if (is_object($avsc) && AvroType::RECORD()->is($avsc->type)) {
             $record = AvroRecord::create($avsc, $namespace);
-            self::$recordCache[$record->name] = $record;
+            self::cacheRecord($record);
             return $record;
         } else if (is_object($avsc) && AvroType::ARRAY()->is($avsc->type)) {
             return AvroArray::create($avsc, $namespace);
@@ -25,6 +26,8 @@ class AvroTypeFactory {
             return AvroUnion::create($avsc, $namespace);
         } else if (key_exists($avsc, self::$recordCache)) {
             return self::$recordCache[$avsc];
+        } else if (key_exists($avsc, self::$namespaceCache)) {
+            return self::$namespaceCache[$avsc];
         } else if (is_string($avsc)) {
             // primitive type
             return new AvroType($avsc);
@@ -41,5 +44,12 @@ class AvroTypeFactory {
         } else {
             throw new NotImplementedException('Unknown Avro Type');
         }
+    }
+
+    private static function cacheRecord(AvroRecord $record) {
+        self::$recordCache[$record->name] = $record;
+
+        $qualifiedName = collect([$record->namespace, $record->name])->filter()->join('.');
+        self::$namespaceCache[$qualifiedName] = $record;
     }
 }
